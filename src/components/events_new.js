aab2cb21ec2e1f03b734ba5fb2da1278854928ee
golazooo23 <kengoteshima0823@gmail.com>
2020-04-18 10:44:12 +0900

    Update events_new.js

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 // import { postEvent } from '../actions'
 
 class EventsNew extends Component {
+  renderField(field){
+    const { input, label, type, meta: {touched, error} } = field
+
+    return (<div></div>)
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <div>foo</div>
-      </React.Fragment>
+      <form>
+        <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+        <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+
+        <div>
+          <input type="submit" value="Submit" disabled={false} />
+          <Link to="/" >Cancel</Link>
+        </div>
+      </form>
     )
   }
 }
 
 // const mapDispatchToProps = ({ postEvents })
 
-export default connect(null, null)(EventsNew)
+
+const validate = values => {
+  const errors = {}
+
+  return errors
+}
+export default connect(null, null)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
 
